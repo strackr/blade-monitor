9e9a15da4f1b29ff069f4d0f07fcc2f415f1b716
Erik <Erik@Erik-PC>
2011-02-07 14:38:11 -0500

    initial commit

---
+++ blade-monitor-core/src/main/java/vcu/blademonitor/RandomMetric/RandomMetric.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+
+public class RandomMetric
+{
+	public MetricStructure createRandomMetric()
+	{
+		MetricStructure myMetric = new MetricStructure("RandomNode");
+		Random generator = new Random();
+		int loopLimit = generator.nextInt(10);
+		
+		String metricName;
+		String metricValue;
+		String metricTime;
+		
+		for(int i = 0; i < loopLimit; i++)
+		{
+			metricName = "RandomeMetric_" + i;
+			myMetric.addMetric(metricName);
+			
+			metricValue = "RandomValue_" + generator.nextInt(10);
+			myMetric.setMetricValue(metricName, metricValue);
+			
+			metricTime = "RandomTime_" + generator.nextInt(20) + 11;
+			myMetric.setMetricTime(metricName, metricTime);
+		}
+		
+		return myMetric;
+	}
+}
