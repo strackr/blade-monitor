53c02d7da39ba9e06dd1e01e7920a9ccc10fa40f
Erik <Erik@Erik-PC>
2011-02-08 15:12:03 -0500

    Declared package and imported MetricStructure

--- blade-monitor-core/src/main/java/vcu/blademonitor/RandomMetric/RandomMetric.java
+++ blade-monitor-core/src/main/java/vcu/blademonitor/RandomMetric/RandomMetric.java
@@ -1,29 +1,30 @@
+package vcu.blademonitor.RandomMetric;
+
 import java.util.Random;
 
-public class RandomMetric
-{
-	public MetricStructure createRandomMetric()
-	{
+import vcu.blademonitor.simpleMonitoringServices.MetricStructure;
+
+public class RandomMetric {
+	public MetricStructure createRandomMetric() {
 		MetricStructure myMetric = new MetricStructure("RandomNode");
 		Random generator = new Random();
 		int loopLimit = generator.nextInt(10);
-		
+
 		String metricName;
 		String metricValue;
 		String metricTime;
-		
-		for(int i = 0; i < loopLimit; i++)
-		{
+
+		for (int i = 0; i < loopLimit; i++) {
 			metricName = "RandomeMetric_" + i;
 			myMetric.addMetric(metricName);
-			
+
 			metricValue = "RandomValue_" + generator.nextInt(10);
 			myMetric.setMetricValue(metricName, metricValue);
-			
+
 			metricTime = "RandomTime_" + generator.nextInt(20) + 11;
 			myMetric.setMetricTime(metricName, metricTime);
 		}
-		
+
 		return myMetric;
 	}
 }
