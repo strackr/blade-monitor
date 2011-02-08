29f50b310ab99188623562cb69bdb4ce4450a7ae
Erik <Erik@Erik-PC>
2011-02-08 15:10:10 -0500

    Bug fixes

--- blade-monitor-core/src/test/java/vcu/blademonitor/simpleMonitoringServices/MetricStructureTest.java
+++ blade-monitor-core/src/test/java/vcu/blademonitor/simpleMonitoringServices/MetricStructureTest.java
@@ -1,9 +1,11 @@
-package vcu.blademonitor;
+package vcu.blademonitor.simpleMonitoringServices;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.Calendar;
+import java.util.Date;
+
 import org.junit.Test;
-import java.util.*;
 
 public class MetricStructureTest {
 
@@ -13,44 +15,36 @@ public class MetricStructureTest {
 		assertTrue(ms.getNodeName().equals("N/A"));
 		assertTrue(ms.listMetricNames().size() == 0);
 	}
-	
+
 	@Test
-	public void test_checkConstructorWithParam()
-	{
+	public void test_checkConstructorWithParam() {
 		MetricStructure ms = new MetricStructure("master1");
 		assertTrue(ms.getNodeName().equals("master1"));
 		assertTrue(ms.listMetricNames().size() == 0);
 	}
 
 	@Test
-	public void test_metricSetup()
-	{
+	public void test_metricSetup() {
 		MetricStructure ms = new MetricStructure("maste1");
 		assertTrue(ms.addMetric("CPU Usage"));
 		assertTrue(ms.setMetricValue("CPU Usage", "0.05"));
-		Calendar calendar = new Calendar();
-		int hour = calendar.get(Calendar.HOUR);
-		int minute = calendar.get(Calendar.MINUTE);
-		int second = calendar.get(Calendar.SECOND);
-		assertTrue(ms.setMetricTime(hour + ":" + minute + ":" + second));
-		
+		Date now = Calendar.getInstance().getTime();
+		assertTrue(ms.setMetricTime("CPU Usage", now.toString()));
+
 		assertTrue(ms.listMetricNames().contains("CPU Usage"));
-		assertTrue(ms.getMetric("CPU Usage").contains("0.05"));
-		assertTrue(ms.getMetric("CPU Usage")
-				.contains(hour + ":" + minute + ":" + second));
+		assertTrue(ms.getMetrics("CPU Usage").contains("0.05"));
+		assertTrue(ms.getMetrics("CPU Usage").contains(now.toString()));
 	}
-	
+
 	@Test
-	public void test_nodeRename()
-	{
+	public void test_nodeRename() {
 		MetricStructure ms = new MetricStructure("master1");
-		ms.setName("slave2");
+		ms.setNodeName("slave2");
 		assertTrue(ms.getNodeName().equals("slave2"));
 	}
-	
+
 	@Test
-	public void test_noDups()
-	{
+	public void test_noDups() {
 		MetricStructure ms = new MetricStructure("master1");
 		ms.addMetric("CPU Usage");
 		assertTrue(ms.addMetric("CPU Usage") == false);
