10db4afb9a6b9e442d3d582b48eaa7e15a1c5422
Erik <Erik@Erik-PC>
2011-02-13 13:51:53 -0500

    Initial commit

---
+++ blade-monitor-core/src/main/java/vcu/blademonitor/simpleMonitoringServices/DebugMeasurementHandler.java
@@ -0,0 +1,41 @@
+package vcu.blademonitor.simpleMonitoringServices;
+
+import java.util.ArrayList;
+
+/**
+ * 
+ * @author Erik - 02/13/2011
+ * 
+ *         DebugMeasurementHandler extends the MeasurementHandler interface and
+ *         implements the handleMeasurement method
+ * 
+ */
+
+public class DebugMeasurementHandler implements MeasurementHandler {
+
+	/**
+	 * @author Erik - 02/13/2011
+	 * 
+	 *         handleMeasurement displays the nodeName, metricNames, values, and
+	 *         times for a given metric, myStructure
+	 * 
+	 * @param - myStructure is the MetricStructure that will have all its
+	 *        metrics displayed to screen
+	 */
+	@Override
+	public void handleMeasurement(MetricStructure myStructure) {
+		System.out.println("Node Name -> " + myStructure.getNodeName());
+
+		ArrayList<String> metricNames = (ArrayList<String>) myStructure
+				.listMetricNames();
+		MetricObject tempObject = null;
+		for (int i = 0; i < metricNames.size(); i++) {
+			tempObject = myStructure.getMetrics(metricNames.get(i));
+			System.out.println("Metric Name  -> " + tempObject.getName());
+			System.out.println("Metric Value -> " + tempObject.getValue());
+			System.out.println("Metric Time  -> " + tempObject.getTime());
+			System.out.println();
+		}
+	}
+
+}
---
+++ blade-monitor-core/src/main/java/vcu/blademonitor/simpleMonitoringServices/MeasurementHandler.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package vcu.blademonitor.simpleMonitoringServices;
+
+/**
+ * @author Erik - 02/13/2011
+ * 
+ *         The interface for handling measurements from the system
+ * 
+ */
+
+public interface MeasurementHandler {
+
+	/**
+	 * @author Erik - 02/13/2011
+	 * 
+	 *         The interface for handleMeasurement.
+	 * 
+	 * @param - myStructure is the metricStructure to be handled
+	 * 
+	 */
+	public void handleMeasurement(MetricStructure myStructure);
+
+}
