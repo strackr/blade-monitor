96dd4bca98d24af938bb7b082c5874466b421489
Erik <Erik@Erik-PC>
2011-02-22 20:11:56 -0500

    - MetricStructureTest - update tests to reflect changes in MetricStructure

--- blade-monitor-core/src/test/java/vcu/blademonitor/monitoring/MetricStructureTest.java
+++ blade-monitor-core/src/test/java/vcu/blademonitor/monitoring/MetricStructureTest.java
@@ -1,21 +1,16 @@
 package vcu.blademonitor.monitoring;
 
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import java.util.Calendar;
-import java.util.Date;
-
+import org.joda.time.DateTime;
 import org.junit.Test;
-
-import vcu.blademonitor.monitoring.MetricStructure;
 
 public class MetricStructureTest {
 
 	@Test
 	public void test_checkConstructor() {
-		MetricStructure ms = new MetricStructure();
-		assertTrue(ms.getNodeName().equals("N/A"));
+		MetricStructure ms = new MetricStructure("node1");
+		assertTrue(ms.getNodeName().equals("node1"));
 		assertTrue(ms.listMetricNames().size() == 0);
 	}
 
@@ -28,11 +23,10 @@ public class MetricStructureTest {
 
 	@Test
 	public void test_metricSetup() {
-		MetricStructure ms = new MetricStructure("maste1");
-		assertTrue(ms.addMetric("CPU Usage"));
-		assertTrue(ms.setMetricValue("CPU Usage", 0.05));
-		Date now = Calendar.getInstance().getTime();
-		assertTrue(ms.setMetricTime("CPU Usage", now));
+		MetricStructure ms = new MetricStructure("master1");
+		DateTime now = new DateTime();
+		MetricObject temp = new MetricObject("CPU Usage", 0.05, now);
+		ms.addMetric(temp);
 
 		assertTrue(ms.listMetricNames().contains("CPU Usage"));
 		assertTrue(ms.getMetrics("CPU Usage").getValue() == 0.05);
@@ -47,20 +41,10 @@ public class MetricStructureTest {
 	}
 
 	@Test
-	public void test_nodeRename() {
-		MetricStructure ms = new MetricStructure("master1");
-		ms.setNodeName("slave2");
-		assertTrue(ms.getNodeName().equals("slave2"));
-	}
-
-	@Test
 	public void test_noDups() {
 		MetricStructure ms = new MetricStructure("master1");
-		ms.addMetric("CPU Usage");
-		assertTrue(ms.addMetric("CPU Usage") == false);
-
-		assertFalse(ms.setMetricValue("nonexistentMetric", 0));
-		assertFalse(ms.setMetricTime("nonexistentMetric", Calendar
-				.getInstance().getTime()));
+		MetricObject temp = new MetricObject("CPU Usage", 0.0, new DateTime());
+		ms.addMetric(temp);
+		assertTrue(ms.addMetric(temp) == false);
 	}
 }
