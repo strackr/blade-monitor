985c0663072064aa9d0241af7275d6d29fc3e5ff
Erik <Erik@Erik-PC>
2011-02-22 20:10:45 -0500

    - MetricObjectTest - Updated the test to reflect changes in the MetricObject structure

--- blade-monitor-core/src/test/java/vcu/blademonitor/monitoring/MetricObjectTest.java
+++ blade-monitor-core/src/test/java/vcu/blademonitor/monitoring/MetricObjectTest.java
@@ -1,15 +1,10 @@
 package vcu.blademonitor.monitoring;
 
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import java.util.Calendar;
-import java.util.Date;
-
+import org.joda.time.DateTime;
 import org.junit.Before;
 import org.junit.Test;
-
-import vcu.blademonitor.monitoring.MetricObject;
 
 public class MetricObjectTest {
 
@@ -18,72 +13,12 @@ public class MetricObjectTest {
 	}
 
 	@Test
-	public final void testMetricObject() {
-		MetricObject myMetric = new MetricObject();
-
-		assertTrue(myMetric.getName() == null);
-		assertTrue(myMetric.getValue() == 0d);
-		assertTrue(myMetric.getTime() == null);
-	}
-
-	@Test
-	public final void testMetricObjectString() {
-		MetricObject myMetric = new MetricObject("Metric");
-
-		assertTrue(myMetric.getName().equals("Metric"));
-		assertTrue(myMetric.getValue() == 0d);
-		assertTrue(myMetric.getTime() == null);
-	}
-
-	@Test
-	public final void testMetricObjectStringDoubleCalendar() {
-		Calendar myCalendar = Calendar.getInstance();
-		Date myDate = myCalendar.getTime();
-		MetricObject myMetric = new MetricObject("Metric", 0.05, myCalendar);
-
-		assertTrue(myMetric.getTime().compareTo(myDate) == 0);
-		assertTrue(myMetric.getName().equals("Metric"));
-		assertTrue(myMetric.getValue() == 0.05);
-	}
-
-	@Test
-	public final void testMetricObjectStringDoubleDate() {
-		Date myDate = Calendar.getInstance().getTime();
+	public final void testMetricObjectStringDoubleDateTime() {
+		DateTime myDate = new DateTime();
 		MetricObject myMetric = new MetricObject("Metric", 0.05, myDate);
 
 		assertTrue(myMetric.getTime() == myDate);
 		assertTrue(myMetric.getName().equals("Metric"));
 		assertTrue(myMetric.getValue() == 0.05);
-	}
-
-	@Test
-	public final void testSetters() {
-		MetricObject myMetric = new MetricObject("Metric", 0.05, (Date) null);
-		myMetric.setName("Bob");
-		myMetric.setValue(10);
-		Date myDate = Calendar.getInstance().getTime();
-		myMetric.setTime(myDate);
-
-		assertTrue(myMetric.getName().equals("Bob"));
-		assertTrue(myMetric.getValue() == 10);
-		assertTrue(myMetric.getTime() == myDate);
-	}
-
-	@Test
-	public final void testSetTimeCalendar() {
-		MetricObject myMetric = new MetricObject("Metric", 0.05, Calendar
-				.getInstance().getTime());
-		Calendar myCalendar = Calendar.getInstance();
-		myMetric.setTime(myCalendar);
-
-		assertTrue(myMetric.getTime() instanceof Date);
-	}
-
-	@Test
-	public final void testResetTime() {
-		MetricObject myMetric = new MetricObject();
-		myMetric.resetTime();
-
-		assertFalse(myMetric.getTime() == null);
 	}
 }
